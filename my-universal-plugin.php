bb45919d3e97f7af2444c2930b5cfc4c2dd47d87
Dmitry <dmitry.bochkov@gmail.com>
2019-01-21 17:36:08 +0200

    added admin page and settings link

--- my-universal-plugin.php
+++ my-universal-plugin.php
@@ -36,8 +36,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 if (! class_exists( 'MUPlugin ') ) {
   class MUPlugin
   {
+    public $plugin;
+
+    function __construct() {
+      $this->plugin = plugin_basename( __FILE__ );
+    }
+
     function register() {
       add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+      add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+
+      add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
+    }
+
+    public function settings_link( $links ) {
+      $settings_link = '<a href="admin.php?page=mu_plugin">Settings</a>';
+      array_push( $links, $settings_link);
+      return $links;
+    }
+
+    public function add_admin_pages() {
+      add_menu_page( 'My Universal Plugin', 'MU Plugin', 'manage_options', 'mu_plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+    }
+
+    public function admin_index() {
+      require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
     }
 
     protected function create_post_type() {
---
+++ templates/admin.php
@@ -0,0 +1,1 @@
+<h1>My Universal Plugin</h1>
