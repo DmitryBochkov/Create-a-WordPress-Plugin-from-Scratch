9d21b947e3b164f949576b51d3d3349c236f3612
Dmitry <dmitry.bochkov@gmail.com>
2019-02-07 21:57:19 +0200

    cleanup - declared the options array in the basecontroller

--- inc/Base/BaseController.php
+++ inc/Base/BaseController.php
@@ -18,15 +18,15 @@ class BaseController
     $this->plugin = plugin_basename( dirname( __FILE__, 3 ) ) . '/my-universal-plugin.php';
 
     $this->managers = array(
-      'cpt_manager',
-      'taxonomy_manager',
-      'media_widget',
-      'gallery_manager',
-      'testimonial_manager',
-      'templates_manager',
-      'login_manager',
-      'membership_manager',
-      'chat_manager',
+      'cpt_manager' => 'Activate CPT Manager',
+      'taxonomy_manager' => 'Activate Taxonomy Manager',
+      'media_widget' => 'Activate Media Widget',
+      'gallery_manager' => 'Activate Gallery Manager',
+      'testimonial_manager' => 'Activate Testimonial Manager',
+      'templates_manager' => 'Activate Templates Manager',
+      'login_manager' => 'Activate Login Manager',
+      'membership_manager' => 'Activate Memberships Manager',
+      'chat_manager' => 'Activate Chat Manager',
     );
   }
 }
