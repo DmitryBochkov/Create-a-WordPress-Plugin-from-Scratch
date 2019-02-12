e3ab779b4ecc2d05707e69fd5e9214ac9833a00c
Dmitry <dmitry.bochkov@gmail.com>
2019-02-12 15:55:20 +0200

    added methods to save default options on activate

--- inc/Api/Callbacks/ManagerCallbacks.php
+++ inc/Api/Callbacks/ManagerCallbacks.php
@@ -28,9 +28,10 @@ class ManagerCallbacks extends BaseController
     $class = $args['class'];
     $option_name = $args['option_name'];
     $checkbox = get_option( $option_name );
+    $checked = isset( $checkbox[$name] ) ? ( $checkbox[$name] ? true : false ) : false;
     $input_str = '';
     $input_str .= '<div class="' . $class . '">';
-    $input_str .= '<input type="checkbox" id="' . $name . '" name="' . $option_name .'[' . $name . '] " value="1" class="" ' . ($checkbox[$name] ? 'checked' : '') . '>';
+    $input_str .= '<input type="checkbox" id="' . $name . '" name="' . $option_name .'[' . $name . '] " value="1" class="" ' . ( $checked ? 'checked' : '') . '>';
     $input_str .= '<label for="' . $name . '">';
     $input_str .= '<div></div>';
     $input_str .= '</label>';
--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -9,5 +9,13 @@ class Activate
 {
   public static function activate() {
     flush_rewrite_rules();
+
+    if ( get_option( 'mu_plugin' ) ) {
+      return;
+    }
+
+    $default = array();
+
+    update_option( 'mu_plugin', $default );
   }
 }
